2353e678eaf131e919d1d70f94b54c85dcd0a5e8
Steven Zeiler <zeiler.steven@gmail.com>
2014-05-23 12:41:24 -0700

    [FEATURE] Add json.parse to get call.

--- lib/gateway_client.rb
+++ lib/gateway_client.rb
@@ -1,45 +1,58 @@
-require "gateway_client/version"
 require "faraday"
 require "openssl"
 require "json"
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-module GatewayClient
+module Gateway
   class Client
     def initialize ip, username, password
       @connection = Faraday.new "https://#{ip}"
       @connection.basic_auth username, password
     end
 
-    def get uri
-      response = @connection.get uri
-      JSON.parse(response.body)
-    end
-    
-    def get_users
-      response = @connection.get '/v1/users'
-      response.body
-    end
-    
-    def get_external_transactions
-      response = @connection.get '/v1/external_transactions'
-      response.body
-    end
-  
-    def get_external_accounts
-      response = @connection.get '/v1/external_accounts'
-      response.body
+    def get uri, params={}
+      response = @connection.get uri, params
+      begin
+        JSON.parse response.body
+      rescue
+        puts "invalid json:: #{response}"
+        response
+      end
     end
 
-    def get_ripple_addresses
-      response = @connection.get '/v1/ripple_addresses'
-      response.body
+    def post uri, params
+      response = @connection.post uri, params
+      begin
+        JSON.parse response.body
+      rescue
+        puts "invalid json:: #{response}"
+        response
+      end
     end
 
-    def get_ripple_transactions
-      response = @connection.get '/v1/ripple_transactions'
-      response.body
+    def get_users params={}
+      get '/v1/users', params
+    end
+
+    def get_user user_id
+      get "/v1/users/#{user_id}"
+    end
+
+    def get_external_transactions params={}
+      get '/v1/external_transactions', params
+    end
+
+    def get_external_accounts params
+      get '/v1/external_accounts', params
+    end
+
+    def get_ripple_addresses params
+      get '/v1/ripple_addresses', params
+    end
+
+    def get_ripple_transactions params
+      get '/v1/ripple_transactions', params
     end
   end
 end
